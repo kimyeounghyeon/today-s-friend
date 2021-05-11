60d94bca7bccff86086ad5f19977d8983585bed2
kimyeonghyeon <ggog940322@gmail.com>
2021-05-11 22:30:06 +0900

    21. 05. 11. 회원 포인트 조회

--- src/member/model/service/MemberService.java
+++ src/member/model/service/MemberService.java
@@ -13,118 +13,120 @@ import static common.JDBCPool.*;
 
 public class MemberService {
 
-   // insert()
-   public int insert(Member vo) {
-      int result = 0;
-      Connection conn = getConnection();
-      try {
-         result = new MemberDao().insert(conn, vo);
-         if (result != 0) {
-            commit(conn);
-         }
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-      close(conn);
-      return result;
-   }
+	// insert()
+	public int insert(Member vo) {
+		int result = 0;
+		Connection conn = getConnection();
+		try {
+			result = new MemberDao().insert(conn, vo);
+			if (result != 0) {
+				commit(conn);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		close(conn);
+		return result;
+	}
 
-   // login()
-   public Member loginAndReadMember(Member vo) {
-      Member result = null;
-      Connection conn = getConnection();
-      try {
-         result = new MemberDao().loginAndReadMember(conn, vo);
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-      close(conn);
-      return result;
-   }
+	// login()
+	public Member loginAndReadMember(Member vo) {
+		Member result = null;
+		Connection conn = getConnection();
+		try {
+			result = new MemberDao().loginAndReadMember(conn, vo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		close(conn);
+		return result;
+	}
 
-   // search()
-   public Member searchIdNPw(Member vo) {
-      Member result = null;
-      Connection conn = getConnection();
-      try {
-         result = new MemberDao().searchIdNPw(conn, vo);
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-      close(conn);
-      return result;
-   }
+	// search()
+	public Member searchIdNPw(Member vo) {
+		Member result = null;
+		Connection conn = getConnection();
+		try {
+			result = new MemberDao().searchIdNPw(conn, vo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		close(conn);
+		return result;
+	}
 
-   // 멤버 포인트 조회
-   public ArrayList<Member> memberPoint(String search) {
-      ArrayList<Member> list = null;
-      Connection conn = getConnection();
-      try {
-         list = new MemberDao().memberPoint(conn, search);
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-      close(conn);
-      return list;
-   }
+	// 멤버 포인트 조회
+	public ArrayList<Member> memberPoint(String search) {
+		ArrayList<Member> list = null;
+		Connection conn = getConnection();
+		try {
+			list = new MemberDao().memberPoint(conn, search);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		close(conn);
+		return list;
+	}
 
-   // 멤버 등급 업
-   public int gradeUp(Member vo, String id, int mpoint) {
-      int result = 0;
-      Connection conn = getConnection();
-      try {
-         result = new MemberDao().gradeUp(conn, vo, id, mpoint);
-         if (result != 0) {
-            commit(conn);
-         }
-      } catch (Exception e) {
-         e.printStackTrace();
-      } finally {
-         close(conn);
-      }
-      return result;
-   }
-   
+	// 멤버 등급 업
+	public int gradeUp(Member vo, String id, int mpoint) {
+		int result = 0;
+		Connection conn = getConnection();
+		try {
+			result = new MemberDao().gradeUp(conn, vo, id, mpoint);
+			if (result != 0) {
+				commit(conn);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(conn);
+		}
+		return result;
+	}
+
 	public int getMemberCount(String search) {
 		Connection conn = getConnection();
 		int result = new MemberDao().getMemberCount(conn, search);
 		close(conn);
 		return result;
 	}
-	
+
 	public List<Member> getMemberByPage(int start, int end, String search) {
-        Connection conn = getConnection();
-        List<Member> list = new MemberDao().getMemberByPage(conn, start, end, search);
-        close(conn);
-        return list;
-    }
+		Connection conn = getConnection();
+		List<Member> list = new MemberDao().getMemberByPage(conn, start, end, search);
+		close(conn);
+		return list;
+	}
 
-
-	   // search()
-	   public Member searchIdNPw(Member vo) {
-	      Member result = null;
-	      Connection conn = getConnection();
-	      try {
-	         result = new MemberDao().searchIdNPw(conn, vo);
-	      } catch (Exception e) {
-	         e.printStackTrace();
-	      }
-	      close(conn);
-	      return result;
-	   }
-	   
-	   
-		// 회원관리
-	      public ArrayList<Member> memberUser(String search){
-	         ArrayList<Member> list = null;
-	         Connection conn = getConnection();
-	         try {
-	            list = new MemberDao().memberUser(conn, search);
-	         } catch (Exception e) {
-	            e.printStackTrace();
-	         }
-	         close(conn);
-	         return list;
-	    }
+	// 회원관리
+	public ArrayList<Member> memberUser(String search) {
+		ArrayList<Member> list = null;
+		Connection conn = getConnection();
+		try {
+			list = new MemberDao().memberUser(conn, search);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		close(conn);
+		return list;
+	}
 	
+	public int memberupdate(Member vo) {
+		Connection conn = getConnection();
+		int result = 0;
+
+		try {
+			result = new MemberDao().updateMember(conn, vo);
+			if (result != 0) {
+				commit(conn);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		close(conn);
+		return result;
+
+	}
+
 }

ab86ad6822b529870d89ae3f024da4d32df8dfa8
elly <ellylee617@gmail.com>
2021-05-15 02:40:56 +0900

    21.05.14 지역별 분류를 위한 locnum 추가, 페이징처리도 새롭게 진행함

--- src/board/controller/BoardListServlet.java
+++ src/board/controller/BoardListServlet.java
@@ -8,11 +8,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.json.simple.JSONObject;
 
 import board.model.service.BoardService;
 import board.model.vo.Board;
+import member.model.vo.Member;
 
 @WebServlet("/page/board/boardread")
 public class BoardListServlet extends HttpServlet {
@@ -38,6 +40,7 @@ public class BoardListServlet extends HttpServlet {
 		final int PAGEBLOCK = 5; // 화면에 나타날 페이지 링크 수 dP) 화면 하단에 1 2 3
 
 		int hobbyId = Integer.parseInt(request.getParameter("hobbyId"));
+
 		BoardService sv = new BoardService();
 
 		int cnt = 0; // 총 글 개수
@@ -48,8 +51,10 @@ public class BoardListServlet extends HttpServlet {
 		} else {
 			search = null; // ""로 들어오면 null로 처리해버림.
 		}
-		
-		cnt = sv.getBoardCount(search, hobbyId);
+
+		int locnum = Integer.parseInt(request.getParameter("locnum"));
+
+		cnt = sv.getBoardCount(search, hobbyId, locnum);
 
 		int pageCnt = (cnt / PAGESIZE) + (cnt % PAGESIZE == 0 ? 0 : 1); // 총 페이지 개수
 		int currentPage = 1; // 현재 페이지. 기본 세팅 1. 클릭되면 바뀌게 됨.
@@ -64,42 +69,43 @@ public class BoardListServlet extends HttpServlet {
 		} else {
 			currentPage = 1;
 		}
-		
+
 		int startPage = 1; // 화면에 나타날 시작 페이지
 		int endPage = 1; // 화면에 나타날 마지막 페이지
 
 		// 문제 구간 생김. currentPage가 pageBlock 배수인 경우 오류 발생 즉, 3,6,9..
 		if (currentPage % PAGEBLOCK == 0) { // currentPage가 pageBlock 배수인 경우
-			if((currentPage / PAGEBLOCK)==1) {
-				startPage = 1;
-			} else {
-				startPage = ((currentPage / PAGEBLOCK) - 1) * PAGEBLOCK + 1;
-			}
+			startPage = ((currentPage / PAGEBLOCK) - 1) * PAGEBLOCK + 1;
 		} else {
-			startPage = (currentPage / PAGEBLOCK) * PAGEBLOCK + 1;
+			if((currentPage / PAGEBLOCK)==0) {
+				startPage=1;
+			}else {
+				startPage = (currentPage / PAGEBLOCK) * PAGEBLOCK + 1;				
+			}
 		}
+		
 		endPage = startPage + PAGEBLOCK - 1;
 		// 총 페이지 개수보다 endPage가 더 클 수 없음.
 		if (endPage > pageCnt) {
 			endPage = pageCnt;
 		}
-		
+
 		int startRnum = 0;
-		if(currentPage==1) {
+		if (currentPage == 1) {
 			startRnum = 1;
-		} else{
-			startRnum = (currentPage-1) * PAGESIZE +1;
-		}
-		
-		int endRnum = startRnum + PAGESIZE - 1;
-		if(endRnum > cnt) {
-			endRnum = cnt; // 마지막 그
+		} else {
+			startRnum = (currentPage - 1) * PAGESIZE + 1;
 		}
 
+		int endRnum = startRnum + PAGESIZE - 1;
+		if (endRnum > cnt) {
+			endRnum = cnt; // 마지막 그
+		}
+	
 		List<Board> list = null;
 		/********** 검색 *************/
 
-		list = sv.getBoardByPage(hobbyId, startRnum, endRnum, search);
+		list = sv.getBoardByPage(hobbyId, locnum, startRnum, endRnum, search);
 
 		System.out.println("search" + search);
 		request.setAttribute("pageCnt", pageCnt);
@@ -108,6 +114,40 @@ public class BoardListServlet extends HttpServlet {
 		request.setAttribute("currentPage", currentPage);
 		request.setAttribute("blist", list);
 		request.setAttribute("search", search);
+		
+		String loc = "";
+		switch(locnum){
+		case 1:
+			loc = "서울";
+			break;
+		case 2:
+			loc = "경기";
+			break;
+		case 3:
+			loc = "인천";
+			break;
+		case 4:
+			loc = "충청";
+			break;
+		case 5:
+			loc = "강원";
+			break;
+		case 6:
+			loc = "경상";
+			break;
+		case 7:
+			loc = "전라";
+			break;
+		case 8:
+			loc = "제주";
+			break;
+		}
+		request.setAttribute("loc", loc);
+		
+		System.out.println("pageCnt"+ pageCnt);
+		System.out.println("startPage"+ startPage);
+		System.out.println("endPage"+ endPage);
+		System.out.println("currentPage"+ currentPage);
 
 		String page = "";
 
@@ -138,6 +178,7 @@ public class BoardListServlet extends HttpServlet {
 			break;
 
 		}
+		
 		request.getRequestDispatcher(page).forward(request, response);
 	}
 }

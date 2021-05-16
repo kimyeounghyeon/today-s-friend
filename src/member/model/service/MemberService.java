ecb49b060dec49784f0c241768e60df818b5a909
elly <ellylee617@gmail.com>
2021-05-17 02:14:20 +0900

    21.05.16 글, 댓글 작성시 포인트 자동 업그레이드 수정

--- src/member/model/service/MemberService.java
+++ src/member/model/service/MemberService.java
@@ -131,12 +131,12 @@ public class MemberService {
 	}
 
 	// update point
-	   public int updateMPoint(Member vo, int mpoint) {
+	   public int updateMPoint(String id, int mpoint) {
 	      Connection conn = getConnection();
 	      int result = 0;
 
 	      try {
-	         result = new MemberDao().updateMPoint(conn, vo, mpoint);
+	         result = new MemberDao().updateMPoint(conn, id, mpoint);
 	         if (result != 0) {
 	            commit(conn);
 	         }

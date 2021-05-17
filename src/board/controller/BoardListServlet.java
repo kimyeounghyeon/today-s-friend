54927ac2f495ac351d036be902cfb7a9b893bff0
elly <ellylee617@gmail.com>
2021-05-17 09:43:40 +0900

    21.05.17 게시판 페이지 통합으로 인한 에러사항 수정

--- src/board/controller/BoardListServlet.java
+++ src/board/controller/BoardListServlet.java
@@ -18,139 +18,119 @@ import member.model.vo.Member;
 
 @WebServlet("/page/board/boardread")
 public class BoardListServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L;
 
-	public BoardListServlet() {
-		super();
-	}
+   public BoardListServlet() {
+      super();
+   }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		execute(request, response);
-	}
+   protected void doGet(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+      execute(request, response);
+   }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		execute(request, response);
-	}
+   protected void doPost(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+      execute(request, response);
+   }
 
-	private void execute(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		final int PAGESIZE = 2; // 한 페이지당 글 수
-		final int PAGEBLOCK = 5; // 화면에 나타날 페이지 링크 수 dP) 화면 하단에 1 2 3
+   private void execute(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+      final int PAGESIZE = 2; // 한 페이지당 글 수
+      final int PAGEBLOCK = 5; // 화면에 나타날 페이지 링크 수 dP) 화면 하단에 1 2 3
 
-		int hobbyId = Integer.parseInt(request.getParameter("hobbyId"));
+      // 로그인 한 멤버의 로컬넘과 해당 게시판의 하비아이디를 받아옴
+      int hobbyId = Integer.parseInt(request.getParameter("hobbyId"));
+      int locnum = Integer.parseInt(request.getParameter("locnum"));
 
-		BoardService sv = new BoardService();
+      BoardService sv = new BoardService();
 
-		int cnt = 0; // 총 글 개수
-		/********** 검색 *************/
-		String search = request.getParameter("search");
+      int cnt = 0; // 총 글 개수
+      /********** 검색 *************/
+      String search = request.getParameter("search");
+      System.out.println(search + "search");
 
-		if (search != null && !search.equals("")) {
-		} else {
-			search = null; // ""로 들어오면 null로 처리해버림.
-		}
+      if (search != null && !search.equals("")) {
+      } else {
+         search = null; // ""로 들어오면 null로 처리해버림.
+      }
 
-		int locnum = Integer.parseInt(request.getParameter("locnum"));
+      // 현재 로그인 되어 있는 회원의 아이디 확인
+      HttpSession session = request.getSession();
+      Member member = (Member)session.getAttribute("member");
+      String userId = member.getId();
+      int gradeId = member.getGradeid();
 
-		cnt = sv.getBoardCount(search, hobbyId, locnum);
+      cnt = sv.getBoardCount(search, hobbyId, locnum);
 
-		int pageCnt = (cnt / PAGESIZE) + (cnt % PAGESIZE == 0 ? 0 : 1); // 총 페이지 개수
-		int currentPage = 1; // 현재 페이지. 기본 세팅 1. 클릭되면 바뀌게 됨.
-		String pageNum = request.getParameter("pageNum");
+      int pageCnt = (cnt / PAGESIZE) + (cnt % PAGESIZE == 0 ? 0 : 1); // 총 페이지 개수
+      int currentPage = 1; // 현재 페이지. 기본 세팅 1. 클릭되면 바뀌게 됨.
+      String pageNum = request.getParameter("pageNum");
 
-		if (pageNum != null && !pageNum.equals("")) { // 클린 된 숫자를 가지고 온다면
-			try {
-				currentPage = Integer.parseInt(pageNum);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} else {
-			currentPage = 1;
-		}
+      if (pageNum != null && !pageNum.equals("")) { // 클린 된 숫자를 가지고 온다면
+         try {
+            currentPage = Integer.parseInt(pageNum);
+         } catch (Exception e) {
+            e.printStackTrace();
+         }
+      } else {
+         currentPage = 1;
+      }
 
-		int startPage = 1; // 화면에 나타날 시작 페이지
-		int endPage = 1; // 화면에 나타날 마지막 페이지
+      int startPage = 1; // 화면에 나타날 시작 페이지
+      int endPage = 1; // 화면에 나타날 마지막 페이지
 
-		// 문제 구간 생김. currentPage가 pageBlock 배수인 경우 오류 발생 즉, 3,6,9..
-		if (currentPage % PAGEBLOCK == 0) { // currentPage가 pageBlock 배수인 경우
-			startPage = ((currentPage / PAGEBLOCK) - 1) * PAGEBLOCK + 1;
-		} else {
-			if((currentPage / PAGEBLOCK)==0) {
-				startPage=1;
-			}else {
-				startPage = (currentPage / PAGEBLOCK) * PAGEBLOCK + 1;				
-			}
-		}
-		
-		endPage = startPage + PAGEBLOCK - 1;
-		// 총 페이지 개수보다 endPage가 더 클 수 없음.
-		if (endPage > pageCnt) {
-			endPage = pageCnt;
-		}
+      // 문제 구간 생김. currentPage가 pageBlock 배수인 경우 오류 발생 즉, 3,6,9..
+      if (currentPage % PAGEBLOCK == 0) { // currentPage가 pageBlock 배수인 경우
+         startPage = ((currentPage / PAGEBLOCK) - 1) * PAGEBLOCK + 1;
+      } else {
+         if((currentPage / PAGEBLOCK)==0) {
+            startPage=1;
+         }else {
+            startPage = (currentPage / PAGEBLOCK) * PAGEBLOCK + 1;            
+         }
+      }
+      
+      endPage = startPage + PAGEBLOCK - 1;
+      // 총 페이지 개수보다 endPage가 더 클 수 없음.
+      if (endPage > pageCnt) {
+         endPage = pageCnt;
+      }
 
-		int startRnum = 0;
-		if (currentPage == 1) {
-			startRnum = 1;
-		} else {
-			startRnum = (currentPage - 1) * PAGESIZE + 1;
-		}
+      int startRnum = 0;
+      if (currentPage == 1) {
+         startRnum = 1;
+      } else {
+         startRnum = (currentPage - 1) * PAGESIZE + 1;
+      }
 
-		int endRnum = startRnum + PAGESIZE - 1;
-		if (endRnum > cnt) {
-			endRnum = cnt; // 마지막 그
-		}
-	
-		List<Board> list = null;
-		/********** 검색 *************/
+      int endRnum = startRnum + PAGESIZE - 1;
+      if (endRnum > cnt) {
+         endRnum = cnt; // 마지막 그
+      }
+   
+      List<Board> list = null;
+      /********** 검색 *************/
 
-		list = sv.getBoardByPage(hobbyId, locnum, startRnum, endRnum, search);
+      list = sv.getBoardByPage(hobbyId, locnum, startRnum, endRnum, search);
 
-		System.out.println("search" + search);
-		request.setAttribute("pageCnt", pageCnt);
-		request.setAttribute("startPage", startPage);
-		request.setAttribute("endPage", endPage);
-		request.setAttribute("currentPage", currentPage);
-		request.setAttribute("blist", list);
-		request.setAttribute("search", search);
-		request.setAttribute("locnum", locnum);
-	
-		System.out.println("pageCnt"+ pageCnt);
-		System.out.println("startPage"+ startPage);
-		System.out.println("endPage"+ endPage);
-		System.out.println("currentPage"+ currentPage);
+      request.setAttribute("pageCnt", pageCnt);
+      request.setAttribute("startPage", startPage);
+      request.setAttribute("endPage", endPage);
+      request.setAttribute("currentPage", currentPage);
+      request.setAttribute("blist", list);
+      request.setAttribute("search", search);
+      request.setAttribute("locnum", locnum);
+      request.setAttribute("hobbyId", hobbyId);
+      request.setAttribute("id", userId);
+      request.setAttribute("cnt", cnt);
+      request.setAttribute("gradeId", gradeId);
+   
+      System.out.println("pageCnt"+ pageCnt);
+      System.out.println("startPage"+ startPage);
+      System.out.println("endPage"+ endPage);
+      System.out.println("currentPage"+ currentPage);
 
-		String page = "";
-
-		switch (hobbyId) {
-
-		case 1:
-			page = "/page/select/exercise.jsp";
-			break;
-
-		case 2:
-			page = "/page/select/movie.jsp";
-			break;
-
-		case 3:
-			page = "/page/select/game.jsp";
-			break;
-
-		case 4:
-			page = "/page/select/food.jsp";
-			break;
-
-		case 5:
-			page = "/page/select/stock.jsp";
-			break;
-
-		case 6:
-			page = "/page/select/freeboard.jsp";
-			break;
-
-		}
-		
-		request.getRequestDispatcher(page).forward(request, response);
-	}
+      request.getRequestDispatcher("/page/select/board.jsp").forward(request, response);
+   }
 }

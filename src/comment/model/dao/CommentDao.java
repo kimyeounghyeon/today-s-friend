74b6dc951a2c5018d29c2c8529399ab45a808d9d
elly <ellylee617@gmail.com>
2021-05-17 16:04:15 +0900

    21.05.17 내 글에 달린 댓글 생성

--- src/comment/model/dao/CommentDao.java
+++ src/comment/model/dao/CommentDao.java
@@ -3,10 +3,6 @@ package comment.model.dao;
 import java.sql.*;
 import java.util.*;
 
-import javax.naming.*;
-import javax.sql.DataSource;
-import common.JDBCPool;
-
 import comment.model.vo.Comment;
 
 public class CommentDao {
@@ -34,40 +30,41 @@ public class CommentDao {
 		}
 	}
 
+	// 글에서 댓글 보기
 	public List<Comment> commentVOList(Connection conn, Comment commentVO) throws SQLException {
-        List<Comment> CommentVOList = null;
-        Comment vo = null;
+		List<Comment> CommentVOList = null;
+		Comment vo = null;
 
-        try {
-            String sql = " SELECT RENO, RECONTENT, ID, TO_CHAR(REDATE, 'yyyy-mm-dd hh24:mi:ss') AS redate "
-                    + " FROM BOARDRE WHERE BNO = ? ORDER BY RENO DESC ";
+		try {
+			String sql = " SELECT RENO, RECONTENT, ID, TO_CHAR(REDATE, 'yyyy-mm-dd hh24:mi:ss') AS redate "
+					+ " FROM BOARDRE WHERE BNO = ? ORDER BY RENO DESC ";
 
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setInt(1, commentVO.getBno());
-            rs = pstmt.executeQuery();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, commentVO.getBno());
+			rs = pstmt.executeQuery();
 
-            CommentVOList = new ArrayList<Comment>();
-            while (rs.next()) {
-                vo = new Comment();
-                vo.setReno(rs.getInt("reno"));
-                vo.setRecontent(rs.getString("recontent"));
-                vo.setId(rs.getString("id"));
-                vo.setRedate(rs.getString("redate"));
-                vo.setBno(commentVO.getBno());
-                CommentVOList.add(vo);
-            }
-        } finally {
-            close();
-        }
-        return CommentVOList;
-    }
+			CommentVOList = new ArrayList<Comment>();
+			while (rs.next()) {
+				vo = new Comment();
+				vo.setReno(rs.getInt("reno"));
+				vo.setRecontent(rs.getString("recontent"));
+				vo.setId(rs.getString("id"));
+				vo.setRedate(rs.getString("redate"));
+				vo.setBno(commentVO.getBno());
+				CommentVOList.add(vo);
+			}
+		} finally {
+			close();
+		}
+		return CommentVOList;
+	}
 
+	// 댓글 추가
 	public int addComment(Connection conn, Comment commentVO) throws SQLException {
 		int result = 0;
 
 		try {
-			String sql = " INSERT INTO BOARDRE" 
-			+ " VALUES(SEQ_COMMENTS.NEXTVAL, ?, ?, DEFAULT, ?) ";
+			String sql = " INSERT INTO BOARDRE" + " VALUES(SEQ_COMMENTS.NEXTVAL, ?, ?, DEFAULT, ?) ";
 
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, commentVO.getRecontent());
@@ -80,7 +77,8 @@ public class CommentDao {
 		}
 		return result;
 	}
-	
+
+	// 댓글 수정
 	public int modComment(Connection conn, Comment commentVO) {
 		int result = 0;
 		String sql = "UPDATE BOARDRE SET RECONTENT = ?, REDATE = DEFAULT WHERE RENO = ? ";
@@ -98,7 +96,8 @@ public class CommentDao {
 
 		return result;
 	}
-	
+
+	// 하나의 댓글 보기
 	public Comment readComment(Connection conn, Comment commentVO) {
 
 		String sql = "SELECT BNO, RENO, RECONTENT, ID, TO_CHAR(REDATE, 'yyyy-mm-dd hh24:mi:ss') AS redate FROM BOARDRE WHERE RENO = ?";
@@ -124,22 +123,86 @@ public class CommentDao {
 		}
 		return resultVO;
 	}
-	
+
+	// 댓글 삭제
 	public int deleteComment(Connection conn, Comment commentVO) {
-        int result = 0;
-        
-        String sql = "DELETE BOARDRE WHERE RENO = ? ";
-        try {
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setInt(1, commentVO.getReno());
-            result = pstmt.executeUpdate();
+		int result = 0;
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            close();
-        }
+		String sql = "DELETE BOARDRE WHERE RENO = ? ";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, commentVO.getReno());
+			result = pstmt.executeUpdate();
 
-        return result;
-    }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+
+		return result;
+	}
+
+	// 내가 글의 댓글 리스트
+	public List<Comment> myReplylist(Connection conn, int startRnum, int endRnum, String id) {
+		List<Comment> list = null;
+		String sql_1 = "(SELECT BNO FROM BOARD WHERE ID = ?)B";
+
+		String sql = "SELECT BNO, RENO, RECONTENT, ID, TO_CHAR(REDATE, 'yyyy-mm-dd hh24:mi:ss') AS redate FROM " 
+		+ " (SELECT ROWNUM N, R.* FROM BOARDRE R," + sql_1 + " "
+		+ "WHERE R.BNO IN B.BNO ORDER BY R.REDATE DESC) " + " WHERE N >= ? AND N <= ? ";
+
+		pstmt = null;
+		rs = null;
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.setInt(2, startRnum);
+			pstmt.setInt(3, endRnum);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				list = new ArrayList<Comment>();
+				do {
+					Comment ResultVO = new Comment();
+					ResultVO.setBno(rs.getInt("bno"));
+					ResultVO.setId(rs.getString("id"));
+					ResultVO.setRecontent(rs.getString("recontent"));
+					ResultVO.setRedate(rs.getString("redate"));
+					ResultVO.setReno(rs.getInt("reno"));
+
+					list.add(ResultVO);
+				} while (rs.next());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return list;
+	}
+
+	// 내가 쓴 글 총 댓글 카운트
+	public int getMyReplyCount(Connection conn, String id) {
+		int cnt = 0;
+		String sql = "SELECT COUNT(*) FROM BOARDRE R, (SELECT BNO FROM BOARD WHERE ID = ?)B WHERE R.BNO IN B.BNO ORDER BY R.REDATE DESC";
+
+		pstmt = null;
+		rs = null;
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				cnt = rs.getInt(1);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return cnt;
+	}
 }
--- src/comment/model/service/CommentService.java
+++ src/comment/model/service/CommentService.java
@@ -3,7 +3,6 @@ package comment.model.service;
 import comment.model.dao.CommentDao;
 import comment.model.vo.Comment;
 
-
 import static common.JDBCPool.*;
 
 import java.sql.Connection;
@@ -16,48 +15,64 @@ public class CommentService {
 		int result = 0;
 		Connection conn = getConnection();
 		result = new CommentDao().addComment(conn, commentVO);
-			if (result != 0) {
-				commit(conn);
-			}
+		if (result != 0) {
+			commit(conn);
+		}
 		close(conn);
 		return result;
 	}
 
-	public List<Comment> commentVOList(Comment commentVO) throws SQLException{
+	public List<Comment> commentVOList(Comment commentVO) throws SQLException {
 		List<Comment> CommentVOList = null;
 		Connection conn = getConnection();
 		CommentVOList = new CommentDao().commentVOList(conn, commentVO);
 		close(conn);
 		return CommentVOList;
 	}
-	
+
 	public int modComment(Comment commentVO) throws SQLException {
 		int result = 0;
 		Connection conn = getConnection();
 		result = new CommentDao().modComment(conn, commentVO);
-			if (result != 0) {
-				commit(conn);
-			}
+		if (result != 0) {
+			commit(conn);
+		}
 		close(conn);
 		return result;
 	}
-	
-	public Comment readComment(Comment commentVO) throws SQLException{
+
+	public Comment readComment(Comment commentVO) throws SQLException {
 		Comment resultVO = null;
 		Connection conn = getConnection();
 		resultVO = new CommentDao().readComment(conn, commentVO);
 		close(conn);
 		return resultVO;
 	}
-	
+
 	public int deleteComment(Comment commentVO) throws SQLException {
-	      int result = 0;
-	      Connection conn = getConnection();
-	      result = new CommentDao().deleteComment(conn, commentVO);
-	         if (result != 0) {
-	            commit(conn);
-	         }
-	      close(conn);
-	      return result;
-	   }
+		int result = 0;
+		Connection conn = getConnection();
+		result = new CommentDao().deleteComment(conn, commentVO);
+		if (result != 0) {
+			commit(conn);
+		}
+		close(conn);
+		return result;
+	}
+
+	public List<Comment> myReplylist(int startRnum, int endRnum, String id) {
+		List<Comment> CommentVOList = null;
+		Connection conn = getConnection();
+		CommentVOList = new CommentDao().myReplylist(conn, startRnum, endRnum, id);
+		close(conn);
+		return CommentVOList;
+	}
+
+	public int getMyReplyCount(String id) {
+		int cnt = 0;
+		Connection conn = getConnection();
+		cnt = new CommentDao().getMyReplyCount(conn, id);
+		close(conn);
+		return cnt;
+	}
 }
